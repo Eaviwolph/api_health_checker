2c902ae086778676d08150eb426e018509b4f0b0
Quentin ESCUDIER <quentin.escudier@epita.fr>
2025-08-22 21:12:01 +0200

    Fix backend cron task started twice because of debug autoreload

--- backend/cron/cron.py
+++ backend/cron/cron.py
@@ -1,10 +1,12 @@
 import logging
 import threading
 import time
+import os
 
 import concurrent
 from api.settings import CRON_TASK_MAX_WORKERS, CRON_TASK_WAIT_TIME
 from endpoints.models import Endpoint
+from django.conf import settings
 
 # Set up logging
 logger = logging.getLogger(__name__)
@@ -14,15 +16,20 @@ instanciated = False
 
 def StartCron():
     global instanciated
+
+    if settings.DEBUG and os.environ.get("RUN_MAIN") != "true":
+        logger.warning("Skipping cron start in autoreloader parent process")
+        return
+
     if instanciated:
         return
     instanciated = True
 
+    logger.info(
+        f"Running cron job every {CRON_TASK_WAIT_TIME} seconds with {CRON_TASK_MAX_WORKERS} workers"
+    )
     def run_cron_job():
         while True:
-            logger.info(
-                f"Running cron job every {CRON_TASK_WAIT_TIME} seconds with {CRON_TASK_MAX_WORKERS} workers"
-            )
             endpoints = Endpoint.objects.all()
 
             def check_endpoint(endpoint: Endpoint):
@@ -32,6 +39,7 @@ def StartCron():
                     f"Endpoint {endpoint.name} responded with status {endpoint.last_status}"
                 )
 
+            logger.info(f"Found {len(endpoints)} endpoints to check")
             # Thread pool of CRON_TASK_MAX_WORKERS workers to optimize requests
             with concurrent.futures.ThreadPoolExecutor(
                 max_workers=CRON_TASK_MAX_WORKERS
